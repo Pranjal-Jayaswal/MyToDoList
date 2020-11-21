a9250bed41ffc51b7b5e6ccc9dcbe72aba30449f
Pranjal <pranjaljsl2910@gmail.com>
2020-11-21 13:06:07 +0530

    page rendered

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,30 @@
+
+let cl = [
+    {
+        description: 'qaaawert',
+        type: '1234567',
+        dueDate:12-12-2020,
+    }, 
+    {
+        description: 'qaawswwwwwawert',
+        type: '1234567',
+        dueDate:12-12-2020,
+    },
+    {
+        description: 'qaaawafaert',
+        type: '1234567',
+        dueDate:12-12-2020,
+    },
+    {
+        description: 'qaaddefaeert',
+        type: '1234567',
+        dueDate:12-12-2020,
+    }
+];
 module.exports.home = function (req, res) {
     return res.render('home', {
-        title: "Home"
+        title: "toDOlist",
+        workk: cl,
     });
+
 }
--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const express=require('express');
 const app=express();
 const port =8000;
 
+const db=require('./config/mongoose');
+const List=require ('./models/listSchema');
+
 //middleware directing requests to routes folder
 app.use('/', require('./routes'));
 //middleware allowing access to static files like css files for home.ejs
 app.use(express.static('myassets'));
-
+var bodyParser = require('body-parser');
+// parse application/json, basically parse incoming Request Object as a JSON Object 
+app.use(bodyParser.json());
 //setting different fields for the created express app
 app.set('view engine','ejs');
 app.set('views','./views');
+
 
 //directing the app to the port(port is a kind of channel) through/on which it will work
 app.listen(port,function(err){
